1c217465bd5ad2e3ea3c495dd474d57c09e3af91
Anthony Pillot <pillot.anthony@gmail.com>
2022-10-15 14:47:26 +0200

    ✨ Add a `navbar` component

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navbar">
+    {{ packageJson.displayName }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import packageJson from "../../package.json";
+</script>
+
+<style scoped>
+.navbar {
+  background-color: var(--color-dark);
+  color: var(--color-light);
+  padding: 1rem;
+}
+</style>
